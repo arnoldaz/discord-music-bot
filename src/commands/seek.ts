b241dff7bf462203768d3ff5bf253b53e83f1892
ArnoldaZ <c.arnas123@gmail.com>
2025-01-30 21:51:57 +0200

    add ability to seek to time string

--- src/commands/seek.ts
+++ src/commands/seek.ts
@@ -1,7 +1,8 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, inlineCode } from "discord.js";
 import { BaseCommand } from "./baseCommand";
 import { Player } from "../player";
+import { convertToSeconds } from "../timeFormat";
 
 /**
  * Command to seek the currently playing song to specific time.
@@ -15,17 +16,24 @@ export class SeekCommand extends BaseCommand {
         this.data = new SlashCommandBuilder()
             .setName("seek")
             .setDescription("Seeks");
-        this.data.addIntegerOption(option => option
-            .setName("seconds")
-            .setDescription("Seeks to that second")
+        this.data.addStringOption(option => option
+            .setName("time")
+            .setDescription("Seeks to that time")
             .setRequired(true)
         );
     }
 
     public async execute(interaction: CommandInteraction): Promise<void> {
         await interaction.deferReply();
-        const seconds = interaction.options.get("seconds", true).value as number;
+        const time = interaction.options.get("time", true).value as string;
+        const seconds = convertToSeconds(time);
+
+        if (seconds === undefined) {
+            await interaction.editReply("Can't seek to invalid time string.");
+            return;
+        }
+
         this._player.seek(seconds);
-        await interaction.editReply(`Seeked to ${seconds} seconds.`);
+        await interaction.editReply(`Seeking to ${inlineCode(time)} (${seconds} seconds).`);
     }
 }
--- src/timeFormat.ts
+++ src/timeFormat.ts
@@ -28,3 +28,31 @@ export function convertToTimeString(duration: Seconds, stripZeroes = true): stri
 
     return hoursString + minutesString + secondsString;
 }
+
+/**
+ * Converts HH:MM:SS format string to number of seconds.
+ * @param time Time string.
+ * @returns Seconds if conversion is possible, undefined otherwise.
+ */
+export function convertToSeconds(time: string): Seconds | undefined {
+    const parts = time.split(":").map(Number);
+
+    if (parts.some(isNaN))
+        return undefined;
+
+    if (parts.some(x => x < 0))
+        return undefined;
+
+    if (parts.length > 3)
+        return undefined;
+
+    if (parts.length == 1)
+        return parts[0];
+
+    const [h, m, s] = parts.length == 3 ? parts : [0, ...parts];
+    
+    if (h < 0 || m < 0 || m >= 60 || s < 0 || s >= 60)
+        return undefined;
+
+    return h * 3600 + m * 60 + s;
+}
